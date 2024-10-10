baf8c403683473cd2b7f7aca2e6ea6cac57884dd
jubiiz <jubiiz.audet@gmail.com>
2024-10-10 12:21:42 -0400

    Setup project for assignment 2.

---
+++ A2-2024/starter-code/shell.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+#include <unistd.h>
+
+#include "shell.h"
+#include "interpreter.h"
+#include "shellmemory.h"
+
+int parseInput(char ui[]);
+
+// Start of everything
+int main(int argc, char *argv[]) {
+    printf("Shell version 1.3 created September 2024\n");
+
+    char prompt = '$';  				// Shell prompt
+    char userInput[MAX_USER_INPUT];		// user's input stored here
+    int errorCode = 0;					// zero means no error, default
+
+    //init user input
+    for (int i = 0; i < MAX_USER_INPUT; i++) {
+        userInput[i] = '\0';
+    }
+    
+    //init shell memory
+    mem_init();
+    while(1) {							
+        if (isatty(0)){
+            printf("%c ", prompt);
+        } else if (feof(stdin)) {
+            mem_deinit();
+            break;
+        }
+        // here you should check the unistd library 
+        // so that you can find a way to not display $ in the batch mode
+        fgets(userInput, MAX_USER_INPUT-1, stdin);
+        errorCode = parseInput(userInput);
+        if (errorCode == -1) exit(99);	// ignore all other errors
+        memset(userInput, 0, sizeof(userInput));
+    }
+
+    return 0;
+}
+
+int wordEnding(char c) {
+    // You may want to add ';' to this at some point,
+    // or you may want to find a different way to implement chains.
+    return c == '\0' || c == '\n' || c == ' ' || c == ';';
+}
+
+int parseInput(char inp[]) {
+    char tmp[200];
+    char *words[100];
+    int ix = 0;
+    int w = 0;
+    int wordlen = 0;
+    int errorCode = 0;
+    for (; inp[ix] == ' ' && ix < 1000; ix++); // skip white spaces
+    while (inp[ix] != '\n' && inp[ix] != '\0' && ix < 1000) {
+        // extract a word
+        for (wordlen = 0; !wordEnding(inp[ix]) && ix < 1000; ix++, wordlen++) {
+            tmp[wordlen] = inp[ix];                        
+        }
+        tmp[wordlen] = '\0';
+        words[w] = strdup(tmp);
+        w++;
+        if (inp[ix] == '\0') break;
+        else if (inp[ix] == ';') { // process chained command
+            errorCode = interpreter(words, w) || errorCode; // if chaining and a command fails, keep that error code
+
+            for (int i = 0; i < w; i++) {
+                free(words[i]);
+                words[i] = 0;
+            }
+            w = 0;
+            ix++; // skip over ';' character
+        }
+
+        ix++;
+    }
+
+    // process last command 
+    errorCode = interpreter(words, w) || errorCode;
+
+    for (int i = 0; i < w; i++) {
+        free(words[i]);
+        words[i] = 0;
+    }
+    return errorCode;
+}
