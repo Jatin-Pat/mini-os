40e7c89e18de690f121d603e03ff8f6170c01407
Jatin Patel <jatin.patel2@mail.mcgill.ca>
2024-10-25 12:58:34 -0400

    finished 1.2.3

--- A1-2024/starter-code/schedulermemory.h
+++ A1-2024/starter-code/schedulermemory.h
@@ -7,11 +7,14 @@
 int process_code_mem_init();
 int process_code_mem_deinit();
 int find_free_pid(int *ppid);
-int create_pcb_for_pid(int pid);
-int load_script_into_memory(char *filename, int pid);
+int create_pcb_for_pid(int pid, int line_count);
+int load_script_into_memory(char *filename, int pid, int *line_count);
 int free_script_memory_at_index(int index);
 int free_pcb_for_pid(int pid);
 int ready_queue_push(int pid);
 int ready_queue_pop(int *ppid);
 int run_scheduler();
+int ready_queue_reorder_sjf();
+int sequential_policy();
+int round_robin_policy();
 #endif
